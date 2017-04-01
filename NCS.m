6425a647ad123974ac46109af148772784e89a54
Arthur Quinn <arthurquinn92@gmail.com>
2017-03-31 23:10:50 -0400

    submission

--- NCS.m
+++ NCS.m
@@ -7,86 +7,78 @@ function B = NCS(x,y)
     c = (n-1)*4;
     
     %A is matrix of coefficients of unknowns in linear equation
-    %Z is right side of equations
+    %Z is vector of right side of equations
     A = zeros(c);
     Z = zeros(c,1);
     
     %idx keeps track of current row in A
     idx = 1;
     
-    %First constraint is s(x1) = y1
-    A(idx,1) = x(1).^3;
-    A(idx,2) = x(1).^2;
-    A(idx,3) = x(1);
-    A(idx,4) = 1;
-    Z(idx) = y(1);    
-    idx = idx + 1;
-    
     %Note: s(xi-) refers to coefficients of xi for left side cubic spline
     %and s(xi+) refers to coefficients of xi for right side of cubic
     %spline.
-    %Next contraints are s(xi-) = yi and s(xi+) = yi for each xi that is
-    %not an endpoint
-    for i = 2:(n-1)      
-        offset = (i-2)*4 + 1;        
-        A(idx,offset) = x(i).^3;
-        A(idx,offset+1) = x(i).^2;
-        A(idx,offset+2) = x(i);
-        A(idx,offset+3) = 1;
-        Z(idx) = y(i);        
-        idx = idx + 1;
+    for i = 1:n
+        offset = (i-1)*4 + 1; 
         
-        A(idx,offset+4) = x(i).^3;
-        A(idx,offset+5) = x(i).^2;
-        A(idx,offset+6) = x(i);
-        A(idx,offset+7) = 1;
-        Z(idx) = y(i);       
-        idx = idx + 1;            
+        %The endpoints constraints are s(x1) = y1 and s(xn) = yn
+        if i == 1 || i == n
+            if i == n
+                offset = offset-4;
+            end
+            A(idx,offset) = x(i).^3;
+            A(idx,offset+1) = x(i).^2;
+            A(idx,offset+2) = x(i);
+            A(idx,offset+3) = 1;
+            Z(idx) = y(i);    
+            idx = idx + 1;
+        else
+            %Next contraints are s(xi-) = yi and s(xi+) = yi for each xi that is
+            %not an endpoint
+            A(idx,offset-4) = x(i).^3;
+            A(idx,offset-3) = x(i).^2;
+            A(idx,offset-2) = x(i);
+            A(idx,offset-1) = 1;
+            Z(idx) = y(i);        
+            idx = idx + 1;
+
+            A(idx,offset) = x(i).^3;
+            A(idx,offset+1) = x(i).^2;
+            A(idx,offset+2) = x(i);
+            A(idx,offset+3) = 1;
+            Z(idx) = y(i);       
+            idx = idx + 1;    
+            
+            %For each xi that is not an endpoint constrain s'(xi-) = s'(xi+) and
+            %s''(xi-) = s''(xi+)
+            A(idx,offset-4) = 3.*x(i).^2;
+            A(idx,offset-3) = 2.*x(i);
+            A(idx,offset-2) = 1;
+            A(idx,offset) = -3.*x(i).^2;
+            A(idx,offset+1) = -2.*x(i);
+            A(idx,offset+2) = -1;
+            %Z(idx) is init to 0      
+            idx = idx + 1;
+
+            A(idx,offset-4) = 6.*x(i);
+            A(idx,offset-3) = 2;
+            A(idx,offset) = -6.*x(i);
+            A(idx,offset+1) = -2;
+            %Z(idx) is init to 0
+            idx = idx + 1; 
+        end
     end
-    
-    %Constrain the right endpoint using s(xn) = yn
-    A(idx,c-3) = x(n).^3;
-    A(idx,c-2) = x(n).^2;
-    A(idx,c-1) = x(n);
-    A(idx,c) = 1;
-    Z(idx) = y(n);   
-    idx = idx + 1;
-    
-    %For each xi that is not an endpoint constrain s'(xi-) = s'(xi+) and
-    %s''(xi-) = s''(xi+)
-    for i = 2:(n-1)
-        offset = (i-2)*4 + 1;  
-        A(idx,offset) = 3.*x(i).^2;
-        A(idx,offset+1) = 2.*x(i);
-        A(idx,offset+2) = 1;
-        A(idx,offset+4) = -3.*x(i).^2;
-        A(idx,offset+5) = -2.*x(i);
-        A(idx,offset+6) = -1;
-        Z(idx) = 0;       
-        idx = idx + 1;
-        
-        A(idx,offset) = 6.*x(i);
-        A(idx,offset+1) = 2;
-        A(idx,offset+4) = -6.*x(i);
-        A(idx,offset+5) = -2;
-        Z(idx) = 0;
-        idx = idx + 1; 
-    end
-    
     
     %Constrain s''(x1) = 0 and s''(xn) = 0
     A(idx,1) = 6.*x(1);
     A(idx,2) = 2;
-    Z(idx) = 0;
+    %Z(idx) is init to 0
     idx = idx + 1;
     
     A(idx,c-3) = 6.*x(n);
     A(idx,c-2) = 2;
-    Z(idx) = 0;
-    idx = idx + 1;
+    %Z(idx) is init to 0
     
     %Solve system of linear equations to find the coefficients for
     %interpolation
     B = A\Z;
-  
 end
--- NNCS.m
+++ NNCS.m
@@ -1,67 +1,74 @@
+%Interpolates a set of (xi, yi) using not-a-knot cubic splines
 function B = NNCS(x,y)
 
+    %n is number of points to interpolate
+    %c is number of constraints needed to inerpolate
     n = length(x);
     c = (n-3)*4;
     
+    %A is matrix of coefficients of unknowns in linear equation
+    %Z is vector of right side of equations
     A = zeros(c);
     Z = zeros(c,1);
     
+    %idx keeps track of current row in A
     idx = 1;
-    
-    A(idx,1) = x(1).^3;
-    A(idx,2) = x(1).^2;
-    A(idx,3) = x(1);
-    A(idx,4) = 1;
-    Z(idx) = y(1);
-    idx = idx + 1;
-    
-    for i = 3:(n-2)
-        offset = (i-3)*4 + 1;
+       
+    for i = 1:n
+        %x2 and x(n-1) are not considered here
+        if i == 2 || i == (n-1)
+            continue      
+        %The endpoints constraints are s(x1) = y1 and s(xn) = yn
+        elseif i == 1 || i == n
+            offset = (i-1)*4 + 1;
+            if i == n
+                offset = c-3;
+            end
+            A(idx,offset) = x(i).^3;
+            A(idx,offset+1) = x(i).^2;
+            A(idx,offset+2) = x(i);
+            A(idx,offset+3) = 1;
+            Z(idx) = y(i);
+            idx = idx + 1;
+        else
+            offset = (i-3)*4+1;
+            %Next contraints are s(xi-) = yi and s(xi+) = yi for each xi that is
+            %not an endpoint or x2 or x(n-1)
+        	A(idx,offset) = x(i).^3;
+            A(idx,offset+1) = x(i).^2;
+            A(idx,offset+2) = x(i);
+            A(idx,offset+3) = 1;
+            Z(idx) = y(i);
+            idx = idx + 1;
+
+            A(idx,offset+4) = x(i).^3;
+            A(idx,offset+5) = x(i).^2;
+            A(idx,offset+6) = x(i);
+            A(idx,offset+7) = 1;
+            Z(idx) = y(i);
+            idx = idx + 1;
+            
+            %For each xi that is not an endpoint constrain s'(xi-) = s'(xi+) and
+            %s''(xi-) = s''(xi+)
+            A(idx,offset) = 3.*x(i).^2;
+            A(idx,offset+1) = 2.*x(i);
+            A(idx,offset+2) = 1;
+            A(idx,offset+4) = -3.*x(i).^2;
+            A(idx,offset+5) = -2.*x(i);
+            A(idx,offset+6) = -1;
+            %Z(idx) is init to 0
+            idx = idx + 1;
         
-        A(idx,offset) = x(i).^3;
-        A(idx,offset+1) = x(i).^2;
-        A(idx,offset+2) = x(i);
-        A(idx,offset+3) = 1;
-        Z(idx) = y(i);
-        idx = idx + 1;
-        
-        A(idx,offset+4) = x(i).^3;
-        A(idx,offset+5) = x(i).^2;
-        A(idx,offset+6) = x(i);
-        A(idx,offset+7) = 1;
-        Z(idx) = y(i);
-        idx = idx + 1;
-        
+            A(idx,offset) = 6.*x(i);
+            A(idx,offset+1) = 2;
+            A(idx,offset+4) = -6.*x(i);
+            A(idx,offset+5) = -2;
+            %Z(idx) is init to 0
+            idx = idx + 1;
+        end
     end
     
-    A(idx,c-3) = x(n).^3;
-    A(idx,c-2) = x(n).^2;
-    A(idx,c-1) = x(n);
-    A(idx,c) = 1;
-    Z(idx) = y(n);
-    idx = idx + 1;
-    
-    for i = 3:(n-2)
-        offset = (i-3)*4 + 1;
-        
-        A(idx,offset) = 3.*x(i).^2;
-        A(idx,offset+1) = 2.*x(i);
-        A(idx,offset+2) = 1;
-        A(idx,offset+4) = -3.*x(i).^2;
-        A(idx,offset+5) = -2.*x(i);
-        A(idx,offset+6) = -1;
-        Z(idx) = 0;
-        idx = idx + 1;
-        
-        A(idx,offset) = 6.*x(i);
-        A(idx,offset+1) = 2;
-        A(idx,offset+4) = -6.*x(i);
-        A(idx,offset+5) = -2;
-        Z(idx) = 0;
-        idx = idx + 1;
-        
-    end
-    
+    %Last two constraints are s(x2) = y2 and s(x(n-1)) = y(n-1)
     A(idx,1) = x(2).^3;
     A(idx,2) = x(2).^2;
     A(idx,3) = x(2);
@@ -74,7 +81,8 @@ function B = NNCS(x,y)
     A(idx,c-1) = x(n-1);
     A(idx,c) = 1;
     Z(idx) = y(n-1);
-    idx = idx + 1;
-    
+
+    %Solve system of linear equations to find the coefficients for
+    %interpolation
     B = A\Z;
 end
